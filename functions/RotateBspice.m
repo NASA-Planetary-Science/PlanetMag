699caadd161461620a1e94ebbad2b099716a0b76
Marshall Styczinski <itsmoosh@gmail.com>
2022-07-25 18:02:55 -0700

    Streamlined rotation matrix multiplication

--- functions/RotateBspice.m
+++ functions/RotateBspice.m
@@ -1,12 +1,9 @@
 function [BxMoon, ByMoon, BzMoon] = RotateBspice(Bx, By, Bz, ets, spkParent, spkMoon)
     
     Bvec = [Bx; By; Bz];
-    npts = length(ets);
     rotMat = cspice_pxform(spkParent, spkMoon, ets);
-    BvecMoon = zeros(3,npts);
-    parfor i=1:npts
-        BvecMoon(:,i) = rotMat(:,:,i) * Bvec(:,i);
-    end
+    BvecMat(:,1,:) = Bvec;
+    BvecMoon = squeeze(pagemtimes(rotMat, BvecMat));
     BxMoon = BvecMoon(1,:);
     ByMoon = BvecMoon(2,:);
     BzMoon = BvecMoon(3,:);
