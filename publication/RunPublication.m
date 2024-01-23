0b20d4bddeb2807a74706dcafd5165e2b6947315
Marshall Styczinski <itsmoosh@gmail.com>
2024-01-23 14:26:10 -0800

    Adjusted escape for non-linking package name

--- publication/RunPublication.m
+++ publication/RunPublication.m
@@ -1,6 +1,6 @@
 function RunPublication
 % Run all scripts needed to produce data and figure products for the publication describing
-% P\lanetMag, DOI TBD.
+% Planet\Mag, DOI TBD.
 
 % Part of the PlanetMag framework for evaluation and study of planetary magnetic fields.
 % Created by Corey J. Cochrane and Marshall J. Styczinski
